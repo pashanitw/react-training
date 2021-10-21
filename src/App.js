b010a5fac4c70cf2dca9d9a08f0f4d301e54a1e6
psk <pasha.nitw@gmail.com>
2021-10-21 19:38:47 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TodoContainer/>
       </header>
     </div>
   );
+}
+
+const TodoContainer = () => {
+    const initialState = [
+        {   id: 1,
+            text: "this is my first item"
+        },
+        {   id: 2,
+            text: "this is my second"
+        },
+        {   id: 3,
+            text: "this is my third"
+        }
+    ]
+    const [todoList, setTodoList] = useState(initialState);
+    return <div style={{backgroundColor: 'blue', display:'flex', flexDirection:'column', alignItems: 'start', padding: 10, height:  300, position:'relative'}}>
+        {
+            todoList.map(item => {
+                return <div>
+                    <p>{item.text}</p>
+                </div>
+            })
+        }
+        <button style={{position:'absolute', bottom: 0, right:0}}>add</button>
+    </div>
 }
 
 export default App;
